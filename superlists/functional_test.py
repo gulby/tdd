8f5bb298b243806fded7fadb30d166f8e71a246e
gulby <gulby@maukistudio.com>
2016-01-26 10:50:46 +0900

    리팩토링

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#에디스는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
 		#해당 웹사이트를 확인하러 갔다.
@@ -31,30 +36,26 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# "공작깃털 사기" 라고 텍스트 상자에 입력한다.
 		# (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
-		inputbox.send_keys(u'공작깃털 사기')
+		input1 = u'공작깃털 사기'
+		inputbox.send_keys(input1)
 		
 		# 엔터키를 치면 페이지가 갱신되고 작업 목록에
-		# "1: 공작싯털 사기" 아이템이 추가된다.
+		# "1: 공작깃털 사기" 아이템이 추가된다.
 		inputbox.send_keys(Keys.ENTER)
-		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(u'1: 공작깃털 사기', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: %s' % input1)
 		
 		# 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
 		# 다시 "공작깃털을 이용해서 그물 만들기" 라고 입력한다 (에디스는 매우 체계적인 사람이다)
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys(u'공작깃털을 이용해서 그물 만들기')
+		input2 = u'공작깃털을 이용해서 그물 만들기'
+		inputbox.send_keys(input2)
 		inputbox.send_keys(Keys.ENTER)
 		
 		# 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(u'1: 공작깃털 사기', [row.text for row in rows])
-		self.assertIn(
-			u'2: 공작깃털을 이용해서 그물 만들기',
-			[row.text for row in rows]
-		)		
+		self.check_for_row_in_list_table('1: %s' % input1)
+		self.check_for_row_in_list_table('2: %s' % input2)
 		
 		# 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
 		# 사이트는 그녀를 위한 특정 URL 을 생성해준다.
