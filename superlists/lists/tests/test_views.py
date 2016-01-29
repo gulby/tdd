1707b61d8fc4d837c70a8bdb13bb848be584a88c
gulby <gulby@maukistudio.com>
2016-01-30 04:28:53 +0900

    setUp() 을 이용하여 중복을 줄이는 리팩터링

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -124,27 +124,26 @@ class ListViewTest(TestCase):
 
 class NewListTest(TestCase):
 
-    def test_validation_errors_are_sent_back_to_hame_page_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape(EMPTY_LIST_ERROR)
-        self.assertContains(response, expected_error)
+    response = None
+
+    def setUp(self):
+        self.response = self.client.post('/lists/new', data={'text': ''})
+        
+    def tearDown(self):
+        pass
 
     def test_invalid_list_items_arent_saved(self):
-        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
         
+    def test_for_invalid_input_status(self):
+        self.assertEqual(self.response.status_code, 200)
+        
     def test_for_invalid_input_renders_home_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
         
     def test_validation_errors_are_shown_on_home_page(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertContains(response, escape(EMPTY_LIST_ERROR))
+        self.assertContains(self.response, escape(EMPTY_LIST_ERROR))
         
     def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(self.response.context['form'], ItemForm)
