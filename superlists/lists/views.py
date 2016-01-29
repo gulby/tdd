a2459537e9ec168efe01cacd2e8f4ad1eaf57b79
gulby <gulby@maukistudio.com>
2016-01-29 16:00:13 +0900

    list view 를 리팩토링해서 신규 아이템 POST를 처리하도록 함

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -11,7 +11,10 @@ def home_page(request):
     
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        new_item = Item.objects.create(text=new_item_text, list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
     
 def new_list(request):
@@ -25,10 +28,4 @@ def new_list(request):
         list_.delete()
         error = u'빈 아이템을 등록할 수 없습니다'
         return render(request, 'home.html', {'error': error})
-    return redirect('/lists/%d/' % (list_.id,))
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    new_item_text = request.POST.get('item_text')
-    new_item = Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/%d/' % (list_.id,))
