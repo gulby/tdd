56310c019797951710eeb34ca43b6667dfe694b6
gulby <gulby@maukistudio.com>
2016-01-30 04:18:09 +0900

    모든 뷰에서 폼 사용하기. 동작 상태로 복귀

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 # Create your views here.
 def home_page(request):
@@ -12,29 +12,20 @@ def home_page(request):
     
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-    
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            new_text = request.POST['text']
-            new_item = Item(text=new_text, list=list_)
-            new_item.full_clean()
-            new_item.save()
-            return redirect('/lists/%d/' % (list_.id,))
-        except ValidationError:
-            error = u'빈 아이템을 등록할 수 없습니다'
-            
-    return render(request, 'list.html', {'list': list_, 'error': error})
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, 'form': form})
     
 def new_list(request):
-    list_ = List.objects.create()
-    new_text = request.POST.get('text')
-    item = Item(text=new_text, list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = u'빈 아이템을 등록할 수 없습니다'
-        return render(request, 'home.html', {'error': error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        new_text = request.POST.get('text')
+        Item.objects.create(text=new_text, list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {'form': form})
