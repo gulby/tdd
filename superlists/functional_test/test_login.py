646687369acf922023693f4d5f77ba4e556b423a
gulby <gulby@maukistudio.com>
2016-02-04 03:37:41 +0900

    다른 FT 에서의 재사용을 위한 test_login 리팩터링

--- superlists/functional_test/test_login.py
+++ superlists/functional_test/test_login.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 import time
 
 from .base import FunctionalTest
-from selenium.webdriver.support.ui import WebDriverWait
 
+TEST_EMAIL = 'gulby@mockmyid.com'
 
 class LoginTest(FunctionalTest):
 
@@ -21,24 +21,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('창을 찾을 수 없습니다')
         
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=10).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-        
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('gulby@mockmyid.com', navbar.text)
-    
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('gulby@mockmyid.com', navbar.text)
-
     def test_login_with_persona(self):
         # 에디스는 superlist 사이트에 접속한다.
         # 그리고 "로그인" 링크를 발견한다.
@@ -52,26 +34,26 @@ class LoginTest(FunctionalTest):
         ## 테스트 이메일로 mockmyid.com 사용
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('gulby@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
         
         # 개인 창을 닫는다.
         self.switch_to_new_window('To-Do lists')
         
         # 로그인된 것을 알 수 있다.
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
         
         # 페이지를 새로고침해서 실제 세션 로그인 상태인 것을 확인한다.
         # 일회성 로그인이 아니다
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
         
         # 새로운 기능에 겁을 먹었다. 반사적으로 '로그아웃'을 클릭한다.
         time.sleep(1)   # javascript 적용 타이밍 확보 때문
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
         
         # 새로고침 후에 '로그아웃' 상태가 계속된다.
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
         
